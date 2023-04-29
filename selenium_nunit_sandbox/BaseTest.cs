2e31a65525fd1235aee7303fdfc4932ab808c8a2
jimmypvu <114040438+jimmypvu@users.noreply.github.com>
2023-04-29 03:08:44 -0700

    added FluentAssertions package

--- selenium_nunit_sandbox/BaseTest.cs
+++ selenium_nunit_sandbox/BaseTest.cs
@@ -1,4 +1,3 @@
-using AngleSharp.Text;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Support.UI;
@@ -6,8 +5,8 @@ using WebDriverManager.DriverConfigs.Impl;
 
 namespace selenium_nunit_sandbox
 {
-    [TestFixture]
-    public class Tests
+    [TestFixture, Description("Setup and Teardown hooks")]
+    public class BaseTest
     {
         public IWebDriver driver;
         public WebDriverWait wait;
@@ -20,12 +19,14 @@ namespace selenium_nunit_sandbox
             new WebDriverManager.DriverManager().SetUpDriver(new ChromeConfig());
 
             ChromeOptions options = new ChromeOptions();
-            //options.AddArgument("--headless");
+            options.AddArgument("--headless");
+            options.AddArgument("--window-size=1920,1080");
 
             driver = new ChromeDriver(options);
             driver.Manage().Window.Maximize();
             driver.Manage().Cookies.DeleteAllCookies();
-            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20);
+            //driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20);
+            Console.WriteLine(driver.Manage().Window.Size);
 
             wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
             wait.PollingInterval = TimeSpan.FromMilliseconds(250);
@@ -34,6 +35,7 @@ namespace selenium_nunit_sandbox
             jse = (IJavaScriptExecutor)driver;
 
             driver.Navigate().GoToUrl(baseUrl);
+
             WaitForPageLoad();
         }
 
@@ -41,107 +43,6 @@ namespace selenium_nunit_sandbox
         public void Teardown()
         {
             driver.Quit();
-        }
-
-        [Test, Description("Verify page URL is correct")]
-        public void VerifyPageUrl()
-        {
-            string pageUrl = driver.Url;
-
-            Console.WriteLine(pageUrl);
-
-            Assert.That(pageUrl, Is.EqualTo(baseUrl), "Page Url should be {0}", baseUrl);
-        }
-
-        [Test, Description("Verify page title is correct")]
-        public void VerifyPageTitle() 
-        {
-            string pageTitle = driver.Title;
-
-            Console.WriteLine(pageTitle);
-
-            Assert.That(pageTitle, Is.EqualTo("Your Store"), "Page title should be 'Your Store'");
-        }
-
-        [Test, Description("Verify search input is displayed")]
-        public void VerifySearchBarIsDisplayed()
-        {
-            IWebElement searchBar = driver.FindElement(By.XPath("//div[@id='entry_217822']//input[@name='search']"));
-
-            Assert.True(searchBar.Displayed);
-        }
-
-        [Test, Description("Verify search autocomplete suggestions are displayed")]
-        public void VerifySearchAutocompleteIsDisplayed()
-        {
-            IWebElement searchBar = driver.FindElement(By.XPath("//div[@id='entry_217822']//input[@name='search']"));
-
-            string searchTerm = "phone";
-
-            searchBar.SendKeys(searchTerm);
-
-            IWebElement autocompleteContainer = wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(By.CssSelector("div#entry_217822 div#search div.dropdown ul[class='dropdown-menu autocomplete w-100']")));
-
-            Assert.True(autocompleteContainer.Displayed, "Autocomplete suggestions should be displayed if any matches for valid search term");
-
-            List<IWebElement> suggestions = autocompleteContainer.FindElements(By.CssSelector("li h4")).ToList();
-
-            foreach(var item in suggestions)
-            {
-                Assert.True(item.Text.ToLower().Contains(searchTerm), "Autocomplete suggestions should contain search term");
-            }
-        }
-
-        [Test, Description("Verify price within tolerance example")]
-        public void VerifyPriceWithinTolerance()
-        {
-            IWebElement searchBar = driver.FindElement(By.XPath("//div[@id='entry_217822']//input[@name='search']"));
-
-            searchBar.SendKeys("iphone");
-            searchBar.SendKeys(Keys.Enter);
-
-            List<IWebElement> searchResultPriceEles = wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.VisibilityOfAllElementsLocatedBy(By.CssSelector("div.product-thumb span.price-new"))).ToList();
-
-            foreach(var result in searchResultPriceEles)
-            {
-                //old syntax for equals within tolerance, can use Is.InRange instead
-                Assert.AreEqual(123, Double.Parse(result.Text.Replace("$", "")), .5);
-            }
-        }
-
-        [Test, Description("User should be able to filter items by price")]
-        public void FilterItemsByPrice()
-        {
-            IWebElement searchBar = driver.FindElement(By.XPath("//div[@id='entry_217822']//input[@name='search']"));
-
-            searchBar.SendKeys(Keys.Enter);
-
-            List<IWebElement> searchResultEles = wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.VisibilityOfAllElementsLocatedBy(By.CssSelector("div.product-thumb"))).ToList();
-
-            Assert.That(searchResultEles.Count, Is.EqualTo(15));
-
-            IWebElement minPriceField = wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(By.CssSelector("div[id='mz-filter-panel-0-0'] input[placeholder='Minimum Price']")));
-
-            minPriceField.Clear();
-            minPriceField.SendKeys("300");
-            minPriceField.SendKeys(Keys.Enter);
-
-            IWebElement maxPriceField = wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(By.CssSelector("div[id='mz-filter-panel-0-0'] input[placeholder='Maximum Price']")));
-
-            maxPriceField.Clear();
-            maxPriceField.SendKeys("400");
-            maxPriceField.SendKeys(Keys.Enter);
-
-            Pause();
-
-            List<IWebElement> filteredResultEles = wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.VisibilityOfAllElementsLocatedBy(By.CssSelector("div.product-thumb"))).ToList();
-
-            Assert.That(filteredResultEles.Count, Is.AtMost(15), "Filtered results should contain at most 15 items");
-
-            foreach(var result in filteredResultEles)
-            {
-                Assert.That(Double.Parse(result.FindElement(By.CssSelector("span.price-new")).Text.Replace("$", "")), Is.InRange(300, 400));
-            }
         }
 
         public bool IsPageLoadComplete()
--- selenium_nunit_sandbox/MiscTests.cs
+++ selenium_nunit_sandbox/MiscTests.cs
@@ -1,12 +1,119 @@
-﻿using System;
+﻿using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FluentAssertions;
 
 namespace selenium_nunit_sandbox
 {
-    internal class Tests
+    internal class MiscTests : BaseTest 
     {
+
+        [Test, Description("Verify page URL is correct")]
+        public void VerifyPageUrl()
+        {
+            string pageUrl = driver.Url;
+
+            Console.WriteLine(pageUrl);
+
+            Assert.That(pageUrl, Is.EqualTo(baseUrl), "Page Url should be {0}", baseUrl);
+        }
+
+        [Test, Description("Verify page title is correct")]
+        public void VerifyPageTitle()
+        {
+            string pageTitle = driver.Title;
+
+            Console.WriteLine(pageTitle);
+
+            pageTitle.Should().Be("Your Store");
+
+            //Assert.That(pageTitle, Is.EqualTo("Your Store"), "Page title should be 'Your Store'");
+        }
+
+        [Test, Description("Verify search input is displayed")]
+        public void VerifySearchBarIsDisplayed()
+        {
+            IWebElement searchBar = driver.FindElement(By.XPath("//div[@id='entry_217822']//input[@name='search']"));
+
+            Assert.True(searchBar.Displayed);
+        }
+
+        [Test, Description("Verify search autocomplete suggestions are displayed")]
+        public void VerifySearchAutocompleteIsDisplayed()
+        {
+            IWebElement searchBar = driver.FindElement(By.XPath("//div[@id='entry_217822']//input[@name='search']"));
+
+            string searchTerm = "phone";
+            searchBar.SendKeys(searchTerm);
+
+            IWebElement autocompleteContainer = wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(By.CssSelector("div#entry_217822 div#search div.dropdown ul[class='dropdown-menu autocomplete w-100']")));
+
+            Assert.True(autocompleteContainer.Displayed, "Autocomplete suggestions should be displayed if any matches for valid search term");
+
+            List<IWebElement> suggestions = autocompleteContainer.FindElements(By.CssSelector("li h4")).ToList();
+
+            foreach (var item in suggestions)
+            {
+                Assert.True(item.Text.ToLower().Contains(searchTerm), "Autocomplete suggestions should contain search term");
+            }
+        }
+
+        [Test, Description("Verify price within tolerance example")]
+        public void VerifyPriceWithinTolerance()
+        {
+            IWebElement searchBar = driver.FindElement(By.XPath("//div[@id='entry_217822']//input[@name='search']"));
+
+            searchBar.SendKeys("iphone");
+            searchBar.SendKeys(Keys.Enter);
+
+            List<IWebElement> searchResultPriceEles = wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.VisibilityOfAllElementsLocatedBy(By.CssSelector("div.product-thumb span.price-new"))).ToList();
+
+            foreach (var result in searchResultPriceEles)
+            {
+                //old syntax for equals within tolerance, can use Is.InRange instead
+                Assert.AreEqual(123, Double.Parse(result.Text.Replace("$", "")), .5);
+            }
+        }
+
+        [Test, Description("User should be able to filter items by price")]
+        public void FilterItemsByPrice()
+        {
+            IWebElement searchBar = driver.FindElement(By.XPath("//div[@id='entry_217822']//input[@name='search']"));
+
+            searchBar.SendKeys(Keys.Enter);
+
+            List<IWebElement> searchResultEles = wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.VisibilityOfAllElementsLocatedBy(By.CssSelector("div.product-thumb"))).ToList();
+
+            Assert.That(searchResultEles.Count, Is.EqualTo(15));
+
+            ////div[@id='mz-filter-panel-0-0']//input[contains(@aria-label, 'Minimum')]
+
+            IWebElement minPriceField = wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(By.CssSelector("div[id='mz-filter-panel-0-0'] input[placeholder='Minimum Price']")));
+
+            minPriceField.Clear();
+            minPriceField.SendKeys("300");
+            minPriceField.SendKeys(Keys.Enter);
+
+            IWebElement maxPriceField = wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(By.CssSelector("div[id='mz-filter-panel-0-0'] input[placeholder='Maximum Price']")));
+
+            maxPriceField.Clear();
+            maxPriceField.SendKeys("400");
+            maxPriceField.SendKeys(Keys.Enter);
+
+            Pause();
+
+            List<IWebElement> filteredResultEles = wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.VisibilityOfAllElementsLocatedBy(By.CssSelector("div.product-thumb"))).ToList();
+
+            Assert.That(filteredResultEles.Count, Is.AtMost(15), "Filtered results should contain at most 15 items");
+
+            foreach (var result in filteredResultEles)
+            {
+                Assert.That(Double.Parse(result.FindElement(By.CssSelector("span.price-new")).Text.Replace("$", "")), Is.InRange(300, 400));
+            }
+        }
     }
 }
